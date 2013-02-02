3d12ae1a2065811957a843b44e8a7e69fafe9232
Chip Stewart <stewart@broadinstitute.org>
2013-02-02 18:48:17 +0000

    test script

---
+++ maf2igv_test.sh
@@ -0,0 +1,3 @@
+maf2igv.sh EwngSRC-SJDES018 /local/cga-fh/cga/Sigma_Pediatric/Individual_Set/AN_Sigma_EwingSarcoma_29Jan2013_TRN31/jobs/mutsig1.0_Sigma_Pediatric/AN_Sigma_EwingSarcoma_29Jan2013_TRN31.final_analysis_set.maf EwngSRC-SJDES018-Tumor EwngSRC-SJDES018-Normal   /seq/picard_aggregation/C594/SJDES018-1/v3/SJDES018-1.bam /seq/picard_aggregation/C594/SJDES018-2/v3/SJDES018-2.bam /seq/picard_aggregation/G14864/SJDES018-1/v2/SJDES018-1.bam /seq/picard_aggregation/G14864/SJDES018-2/v1/SJDES018-2.bam /xchip/cga/gdac-prod/genepattern/jobResults/4872146/EwngSRC-SJDES018-Tumor.recalibrated.bam - - snapshot hg19 800 70 12345 1234  3500
+
+
